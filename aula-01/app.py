6198097d9f24f173185d84704e879dbef26719a9
milliezm <milliezmorelli@gmail.com>
2025-02-18 11:40:51 -0300

    @milliezm

--- aula-01/app.py
+++ aula-01/app.py
@@ -1,15 +1,33 @@
 # Importando o Flask
-from flask import Flask
+from flask import Flask, render_template
+# render_template serve para carregar as páginas
 
 # Carregando o Flask na variável app
-app = Flask(__name__)
+app = Flask(__name__, template_folder='views')
+# template_folder é a pasta em que as páginas estão
 
 # Criando a primeira rota do site
 @app.route('/')
 # Criando função no Python
 def home():
-    return '<h1>Bem-vindo ao meu primeiro site em Flask!</h1>'
+    return render_template('index.html')
+
+# rota de games
+@app.route('/games')
+def games():
+    titulo = 'CS-GO'
+    ano = 2012
+    categoria = 'FPS Online'
+    jogadores = ['Miguel José', 'Miguel Isack', 'Leaf', 'Quemario', 'Trop', 'Aspax', 'maxxdiego']
+    jogos = ['Fortnite', 'Minecraft', 'Stray', 'The Legend of Zelda: Breath of The Wild', 'Baldurs Gate 3', 'Taiko no Tatsujin']
+    return render_template('games.html',
+                           titulo=titulo,
+                           ano=ano,
+                           categoria=categoria,
+                           jogadores=jogadores,
+                           jogos=jogos)
 
 # Iniciando o servidor no localhost, porta 5000, modo de depuração ativado
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+# para deixar o seu projeto disponivel na rede, trocar o 'localhost' por '0.0.0.0' e disponibilizar o ip da sua máquina para quem quiser acessar
