fe2769b84c6b5ce5a9275598823a13ebccca4c35
milliezm <milliezmorelli@gmail.com>
2025-05-13 10:56:09 -0300

    @milliezm

--- aula-04-crud-com-mysql/controllers/routes.py
+++ aula-04-crud-com-mysql/controllers/routes.py
@@ -49,16 +49,16 @@ def init_app(app):
                                gamelist=gamelist)
 
     # rota de estoque (crud)
-    @app.route('/estoque', methods=['GET', 'POST'])
-    @app.route('/estoque/<int:id>')
-    def estoque(id=None):
+    @app.route('/estoquegames', methods=['GET', 'POST'])
+    @app.route('/estoquegames/<int:id>')
+    def estoquegames(id=None):
         # verificar se foi enviado alguma ID
         if id:
             # buscando jogo pela id e deletando
             game = Game.query.get(id)
             db.session.delete(game)
             db.session.commit()
-            return redirect(url_for('estoque'))
+            return redirect(url_for('estoquegames'))
             
         # verificando se a requisição é POST
         # = -> atribuição
@@ -72,11 +72,28 @@ def init_app(app):
             db.session.add(newgame)
             # confirmando as alterações
             db.session.commit()
-            return redirect(url_for('estoque'))
+            return redirect(url_for('estoquegames'))
 
         # fazendo um select no banco (pegando todos os jogos da tabela) / SELECT * from games
         gamesestoque = Game.query.all()
+        return render_template('estoquegames.html',
+                               gamesestoque=gamesestoque)
+        
+    @app.route('/estoqueconsoles', methods=['GET', 'POST'])
+    @app.route('/estoqueconsoles/<int:id>')
+    def estoqueconsoles(id=None):
+        if id:
+            console = Console.query.get(id)
+            db.session.delete(console)
+            db.session.commit()
+            return redirect(url_for('estoqueconsoles'))
+        
+        if request.method == 'POST':
+            newconsole = Console(request.form['nome'], request.form['fabricante'], request.form['preco'], request.form['quantidade'])
+            db.session.add(newconsole)
+            db.session.commit()
+            return redirect(url_for('estoqueconsoles'))
+        
         consoleestoque = Console.query.all()
-        return render_template('estoque.html',
-                               gamesestoque=gamesestoque,
+        return render_template('estoqueconsoles.html',
                                consoleestoque=consoleestoque)
