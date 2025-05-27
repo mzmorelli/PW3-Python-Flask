930eea67c99b17bdf6682fdcdbb5212c627d95ba
milliezm <milliezmorelli@gmail.com>
2025-05-27 11:17:06 -0300

    @milliezm

--- aula-04-crud-com-mysql/controllers/routes.py
+++ aula-04-crud-com-mysql/controllers/routes.py
@@ -116,3 +116,18 @@ def init_app(app):
             return redirect(url_for('estoquegames'))
                     
         return render_template('editgame.html', game=game)
+    
+    
+    @app.route('/editconsole/<int:id>', methods=['GET', 'POST'])
+    def editconsole(id):
+        console = Console.query.get(id)
+        
+        if request.method == 'POST':
+            console.nome = request.form['nome']
+            console.fabricante = request.form['fabricante']
+            console.preco = request.form['preco']
+            console.quantidade = request.form['quantidade']
+            db.session.commit()
+            return redirect(url_for('estoqueconsoles'))
+        
+        return render_template('editconsole.html', console=console)
